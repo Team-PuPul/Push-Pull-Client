e3f805e641b85aa4bef50c4636903cf0f4cf8683
wkdrudgnsdla <s24032@gsm.hs.kr>
2026-05-19 18:02:48 +0900

    Fix Edgegap editor references in player build

--- Assets/Mirror/Transports/Edgegap/EdgegapLobby/LobbyServiceCreateDialogue.cs
+++ Assets/Mirror/Transports/Edgegap/EdgegapLobby/LobbyServiceCreateDialogue.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Threading;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 #if UNITY_EDITOR
 namespace Edgegap
--- Assets/Mirror/Transports/Edgegap/EdgegapLobby/LobbyTransportInspector.cs
+++ Assets/Mirror/Transports/Edgegap/EdgegapLobby/LobbyTransportInspector.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
 using System.Reflection;
 using kcp2k;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 #if UNITY_EDITOR
 namespace Edgegap
