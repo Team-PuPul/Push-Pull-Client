c04d8d43a3f5ed8e6ccc9cf84ad00449b7c4ca12
Lee Siwoo <s24028@gsm.hs.kr>
2026-05-22 12:01:49 +0900

    Revert "chore: 미사용 스크립트 및 프리팹 제거"
    This reverts commit d7ab695db3a13248d8deff4da27e5600f54bf8cb.

---
+++ Assets/Scripts/UI/Main.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+// 필요없는 스크립트
+// 추후 삭제할 것
+public class Main : MonoBehaviour
+{
+	private bool isSelected = false;
+	public Button[] btns;
+	private void Update()
+	{
+		if (Input.anyKey && !isSelected)
+		{
+			btns[0].Select();
+			isSelected = true;
+		}
+	}
+}
