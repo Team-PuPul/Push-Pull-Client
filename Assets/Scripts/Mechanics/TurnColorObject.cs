7bb503082d91a39cb8e251d54619a54985acd54a
wkdrudgnsdla <s24032@gsm.hs.kr>
2026-06-08 19:39:03 +0900

    fix:제미나이 코드리뷰 반영

--- Assets/Scripts/Mechanics/TurnColorObject.cs
+++ Assets/Scripts/Mechanics/TurnColorObject.cs
@@ -57,6 +57,9 @@ public class TurnColorObject : NetworkBehaviour
     private void TurnColorOnServer()
     {
         isWhiteActive = !isWhiteActive;
+
+        ApplyColorState(isWhiteActive);
+
         RpcPlayTurnColorEffect();
     }
 
